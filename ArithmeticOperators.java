27fc44ebf9ee08344eb633f1f0dda33bd3c862ae
Soumadeep <deepmicrosoft2022@gmail.com>
2025-04-06 08:46:38 +0530

    Arithmetic Operators in Java

---
+++ ArithmeticOperators.java
@@ -0,0 +1,52 @@
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        
+        /*
+
+        Arithmetic Operators in Java
+        
+        int number1 = 10;
+        int number2 = 20;
+
+        1. Addition Operator (+)
+
+        int addition = number1 + number2;
+
+        2. Substraction Operator (-)
+
+        int substraction = number1 - number2;
+
+        3. Multiplycation Operator (*)
+
+        int multiply = number1 * number2;
+
+        4. Division Operator (/)
+
+        int divide = number2 / number1;
+
+        5. Modulo Operator (%)
+        
+        int remainder = number2 % number1;
+        
+        Print the ans
+
+        System.out.println(addition);
+
+        System.out.println(substraction);
+
+        System.out.println(multiply);
+
+        System.out.println(divide);
+
+        System.out.println(remainder);
+        
+        
+       
+        
+
+         */
+
+         
+
+    }
+}
